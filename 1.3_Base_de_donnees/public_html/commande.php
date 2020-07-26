19915c1619b43f4f29bb5a274ae540e770cfd048
jocelin devalette <jocelin.devalette@egd.mg>
2020-07-26 10:27:06 +0300

    V0.4 sans vÃrification

--- 1.3_Base_de_donnees/public_html/commande.php
+++ 1.3_Base_de_donnees/public_html/commande.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html lang="fr">
-		
+<html lang="fr">	
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <link rel="stylesheet" href="css/cooperative.css">
-    <script src="javascript/morpion.js"  type="text/javascript"></script>
+    <script src="javascript/cooperative.js"  type="text/javascript"></script>
   </head>
   <body>
       <header>
@@ -17,7 +16,17 @@
 		  
       </header>
       <main>
-	  <?php
+	  
+        <div class='shadowbox' align='center'>
+			<h3 onmouseover="couleurtitre()">Bienvenue</h3>
+			Nous sommes le <?php echo date("d:m:y")?> et il est <?php echo date("H:i:s");?>
+			<br/>
+			votre adresse IP est  <?php echo $_SERVER['REMOTE_ADDR'];?><br/>
+			Ce script est exécuté sur le serveur <?php echo $_SERVER['SERVER_SOFTWARE'];?> qui se 
+			trouve à l'adresse <?php echo $_SERVER['SERVER_ADDR']?>.<br/>
+			Vous appartenez au groupe <?php echo $_GET['groupe']; ?> <br>
+		</div>
+		<?php
 			$servername = 'localhost';
 			$username = $_GET['groupe'];
 			$password = 12345 ;
@@ -30,30 +39,8 @@
             if($conn->connect_error){
                 die('Erreur : ' .$conn->connect_error);
             }
-            $foo = false;
-			$sql = "SELECT email, mot_de_passe FROM buveurs";
-			$resultat = mysqli_query($conn, $sql);
-			if (mysqli_num_rows($resultat) > 0) {
-			// on parse pour vérifier
-			while($row = mysqli_fetch_assoc($result)) {
-					if ($row['email'] == $_GET['login'] and $row['mot_de_passe'] == (int)$_GET['password'])
-					{$foo = true; }
-					}
-			} 
-			
-			// si l'utilisateur n'a pas été identifié on revient à l'index.
-			if ($foo == false)  { echo 'Connexion non réussie'; }
-			else { echo 'Connexion échouée'} ;
+            
         ?>
-        <div class='shadowbox'>
-			<h3 onmouseover="couleurtitre()">Bienvenue</h3>
-			Nous sommes le <?php echo date("d:m:y")?> et il est <?php echo date("H:i:s");?>
-			<br/>
-			votre adresse IP est  <?php echo $_SERVER['REMOTE_ADDR'];?><br/>
-			Ce script est exécuté sur le serveur <?php echo $_SERVER['SERVER_SOFTWARE'];?> qui se 
-			trouve à l'adresse <?php echo $_SERVER['SERVER_ADDR']?>.<br/>
-			Vous appartenez au groupe <?php echo $_GET['groupe']; ?> <br>
-		</div>
 		<br/>
 
       </main>
